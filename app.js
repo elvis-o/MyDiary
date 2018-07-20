2a66a1931085bed8f0f091cb9b071232d194196d
elvis-o <osamagbe.elvis@gmail.com>
2018-07-20 21:34:58 +0100

    Created the get-all-entries-endpoint with test

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+//var router = require('express').Router();
+var bodyParser = require('body-parser');
+var controller = require('./controller.js');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var apiRoutes = require('./router');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get('port'), function(){
+    console.log('server is listening on port'+ server.address().port);
+});
+
+module.exports = {app:app, server:server};
---
+++ data-source.js
@@ -0,0 +1,9 @@
+var entries = [
+  {id : 1, title : "My first diary date", details: "My first date with Janey was remarkable, it went so well we had many more."},
+  {id : 2, title : "PES 2018", details: "I kinda dislike this version of the game, its sluggish and slow"},
+  {id : 3, title : "Presentation", details: "I was called for an impromptu presentation, i lost my cool and almost fainted"}
+];
+
+module.exports = {
+    entries : entries
+}
