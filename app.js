be0a532e4220bc753a23f7f3f2fe27b884f5fc36
elvis-o <osamagbe.elvis@gmail.com>
2018-07-21 13:56:17 +0100

    swapped index router

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var apiRoutes = require('./router');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- router.js
+++ router.js
@@ -3,9 +3,9 @@ var controller = require('./controller.js');
 
 
 router.get('/', controller.indexApp);
-router.get('/v1/entries', controller.getAllEntries);
-router.get('/v1/entries/:entryId', controller.getEntryById);
-router.post('/v1/entries', controller.createNewEntry);
-router.put('/v1/entries/:entryId', controller.editEntry);
+router.get('/api/v1/entries', controller.getAllEntries);
+router.get('/api/v1/entries/:entryId', controller.getEntryById);
+router.post('/api/v1/entries', controller.createNewEntry);
+router.put('/api/v1/entries/:entryId', controller.editEntry);
 
 module.exports = router;
